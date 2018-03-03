9905d165090027fe1366fee11682125d3720c824
arturgromenkov <36747104+arturgromenkov@users.noreply.github.com>
2018-03-03 18:45:04 +0300

    Третий тест

--- Project V3.1a/unit10.pas
+++ Project V3.1a/unit10.pas
@@ -14,18 +14,22 @@ type
 
   TForm7 = class(TForm)
     Edit1: TEdit;
+    ITOG: TLabel;
+    ITOGT1: TLabel;
+    ITOGT2: TLabel;
     Label1: TLabel;
     Label15: TLabel;
     Label2: TLabel;
-    QUES1: TLabel;
+    Label3: TLabel;
     PageControl1: TPageControl;
     Panel1: TPanel;
+    QUES1: TLabel;
     TabSheet1: TTabSheet;
     TabSheet2: TTabSheet;
-    TabSheet3: TTabSheet;
     Timer1: TTimer;
     Timer2: TTimer;
     procedure FormCreate(Sender: TObject);
+    procedure FormHide(Sender: TObject);
     procedure FormShow(Sender: TObject);
 
     procedure Pan1MouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
@@ -58,18 +62,24 @@ implementation
 
 procedure TForm7.FormCreate(Sender: TObject);
 begin
-AssignFile(F1, 'A101.txt');
+AssignFile(F1, 'tests/A101.txt');
  Reset(F1);
 readln(F1,C);
 readln(F1,Ques);
 readln(F1,ANSWER);
 QUES1.Caption:=Ques;
-b:=-1;
+b:=0;
+end;
+
+procedure TForm7.FormHide(Sender: TObject);
+begin
+ Timer1.Enabled:=False;
 end;
 
 procedure TForm7.FormShow(Sender: TObject);
 begin
   Timer1.Enabled:=True;
+  Timer2.Enabled:=true;;
 end;
 
 procedure TForm7.Pan1MouseMove(Sender: TObject; Shift: TShiftState; X,
@@ -111,8 +121,10 @@ begin
     PageControl1.Tag:=PageControl1.Tag+1;
     end;
      if PageControl1.Tag=C then
-     PageControl1.TabIndex:=1
-
+     PageControl1.TabIndex:=1 ;
+      b:=0;
+     Timer1.enabled:=false;
+     Timer1.enabled:=True;
     end;
 
 procedure TForm7.QUES1Click(Sender: TObject);
@@ -134,11 +146,15 @@ begin
     end;
      if PageControl1.Tag=C then
      PageControl1.TabIndex:=1 ;
+
 end;
 
 procedure TForm7.Timer2Timer(Sender: TObject);
 begin
-b:=b+1;
+   if b<>60 then
+b:=b+1
+   else
+   b:=1;
 Label2.caption:=InttoStr(b);
 end;
 
